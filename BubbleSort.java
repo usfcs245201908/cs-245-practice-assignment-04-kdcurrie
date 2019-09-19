32f93102b25f1d1c44f1a3dad1ebb929c2dcb47c
Keaton Currie <kdcurrie@dons.usfca.edu>
2019-09-19 16:13:14 -0700

    Finished

---
+++ BubbleSort.java
@@ -0,0 +1,18 @@
+public class BubbleSort implements SortingAlgorithm {
+    @Override
+    public void sort(int[] a) {
+        for(int i=0;i<a.length-1;i++) {
+            for(int j=0;j<a.length-1-i;j++) {
+                if (a[j]>a[j+1]) {
+                    swap(a,j,j+1);
+                }
+            }
+        }
+
+    }
+    void swap(int[] a,int x,int y) {
+        int temp = a[x];
+        a[x] = a[y];
+        a[y] = temp;
+    }
+}
---
+++ SelectionSort.java
@@ -0,0 +1,16 @@
+public class SelectionSort implements SortingAlgorithm {
+    @Override
+    public void sort(int[] a) {
+        for(int i=0; i<a.length-1; i++) {
+            int min = i;
+            for (int j=i+1;j<a.length;j++) {
+                if(a[j] < a[min]) {
+                    min = j;
+                }
+            }
+            int temp = a[min];
+            a[min] = a[i];
+            a[i] = temp;
+        }
+    }
+}
